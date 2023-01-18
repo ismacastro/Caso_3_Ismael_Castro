c466334122f71c0e7e70a36cf6a741367c0f89e7
Ismael Castro <sima@pop-os.localdomain>
2023-01-18 12:03:40 +0100

    menu entero listo y responsive

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 //Reload window
 
-//setInterval('window.location.reload()', 2000);
+// setInterval('window.location.reload()', 2000);
 
 //Responsive menu
 function updateMenu(){
@@ -9,7 +9,7 @@ function updateMenu(){
 
     if (window.innerWidth < 768) {
         menuLarge.style.display= "none"
-        menu.style.display= "block"
+        menu.style.display= "flex"
     } else {
         menuLarge.style.display= "flex"
         menu.style.display= "none"
@@ -19,3 +19,23 @@ function updateMenu(){
 document.addEventListener('DOMContentLoaded', updateMenu);
 
 window.addEventListener('resize', updateMenu);
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    
+    const buttonMenu = document.getElementById('button-menu');
+    const smallMenu = document.getElementById('menu-small');
+    smallMenu.style.display = "none";
+
+
+    function showMenu() {
+
+     if (smallMenu.style.display === "none"){
+        smallMenu.style.display =  "flex"
+     } else {
+         smallMenu.style.display = "none"
+        }
+    }
+
+    buttonMenu.addEventListener("click", showMenu);
+});
